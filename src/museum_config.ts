d807b079b3f62d807d37f82e3ec5360b03b9498b
Harshit Mittal <80350727+HarshitMittal01@users.noreply.github.com>
2022-07-15 22:44:05 +0530

    bird-field

--- src/museum_config.ts
+++ src/museum_config.ts
@@ -119,7 +119,7 @@ export const museumConfig1 =  {
 
 export const museumConfig2 =  {
     //SCENE FACING: "NORTH"/"EAST"/"SOUTH"/"WEST"
-    sceneOrientation: "NORTH",
+    sceneOrientation: "WEST",
     logo: {
         bottom: {
             //image should be 512px square
@@ -149,6 +149,125 @@ export const museumConfig2 =  {
             width: 512,
             height: 512,
             position: new Vector3(6.8, 2.6, 4.2)
+        },
+        {
+            name: "image 3",
+            imgSrc: "images/003.png",
+            link: "",
+            width: 512,
+            height: 256,
+            position: new Vector3(-6.8, 2.6, 4.2)
+        },
+        {
+            name: "image 4",
+            imgSrc: "images/004.png",
+            link: "",
+            width: 512,
+            height: 512,
+            position: new Vector3(-6.8, 2.6, -4.2)
+        },
+        {
+            name: "image 5",
+            imgSrc: "images/005.png",
+            link: "",
+            width: 512,
+            height: 512,
+            position: new Vector3(6.8, 10, 4.2)
+        },
+        {
+            name: "image 6",
+            imgSrc: "images/006.png",
+            link: "",
+            width: 256,
+            height: 512,
+            position: new Vector3(6.8, 10, -4.2)
+        },
+        {
+            name: "image 7",
+            imgSrc: "images/007.png",
+            link: "",
+            width: 512,
+            height: 256,
+            position: new Vector3(-6.8, 10, 4.2)
+        },
+        {
+            name: "image 8",
+            imgSrc: "images/008.png",
+            link: "",
+            width: 512,
+            height: 512,
+            position: new Vector3(-6.8, 10, -4.2)
+        }
+    ],
+    socialMedia: [
+        {
+            name: "HOMEPAGE",
+            model: "models/social_media/homepage.glb",
+            link: "https://www.decentraland.org",
+            position: new Vector3(6, 1.2, 0.7)
+        },
+        {
+            name: "DISCORD",
+            model: "models/social_media/discord.glb",
+            link: "https://www.discord.org",
+            position: new Vector3(6, 1.2, -0.7)
+        },
+        {
+            name: "TWITTER",
+            model: "models/social_media/twitter.glb",
+            link: "https://www.twitter.com",
+            position: new Vector3(-6, 1.2, 0.7)
+        },
+        {
+            name: "TELEGRAM",
+            model: "models/social_media/telegram.glb",
+            link: "https://www.telegram.org",
+            position: new Vector3(-6, 1.2, -0.7)
+        }
+    ],
+    videoScreen: {
+        src: 'https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875',
+        width: 1280,
+        height: 720
+    },
+    sculpture: {
+        modelSrc: "models/museum/sculpture.glb",
+        position: new Vector3(0, 0.4, 1.5)
+    }
+}
+
+export const museumConfig3 =  {
+    //SCENE FACING: "NORTH"/"EAST"/"SOUTH"/"WEST"
+    sceneOrientation: "SOUTH",
+    logo: {
+        bottom: {
+            //image should be 512px square
+            imgSrc: "images/logo.png",
+            position: new Vector3(0, 5.8883, 6.9747)
+        },
+        top: {
+            name: "BRAND\nNAME",
+            fontSize: 10,
+            color: new Color3(0.1, 0.1, 0.1),
+            position: new Vector3(0, 10.8, 7.15)
+        }
+    },
+    nftImage: [
+        {
+            name: "Small 12 L Backpack WATERPROOF CASUAL MINI BACKPACK BAGS FOR COLLEGE, SCHOOL, TRAVELLING, STUDENTS",
+            imgSrc: "https://rukminim1.flixcart.com/image/880/1056/l55nekw0/backpack/x/5/a/14-waterproof-casual-mini-backpack-bags-for-college-school-original-imagfw52ghxpw5kf.jpeg?q=50",
+            link: "https://www.flipkart.com/krizler-waterproof-casual-mini-backpack-bags-college-school-travelling-students-12-l/p/itm47f8b1bccee2b?pid=BKPGFW52MGVDAJRE&lid=LSTBKPGFW52MGVDAJREDHCYH4&marketplace=FLIPKART&store=reh%2F4d7%2Fak9&srno=b_1_1&otracker=clp_omu_Fashion%2BTop%2BDeals_2_2.dealCard.OMU_offers-store_offers-store_67UF561LATP0_2&otracker1=clp_omu_PINNED_neo%2Fmerchandising_Fashion%2BTop%2BDeals_NA_dealCard_cc_2_NA_view-all_2&fm=neo%2Fmerchandising&iid=en_uEHybzCUY79zen6IC6GVHtdvHwOQJ%2F4gJg%2Bh%2B88iu3PGcnOo%2B6sY7721Mg6C53dGnQuuRm4HSZBwfWmKkbSSLg%3D%3D&ppt=browse&ppn=browse&ssid=6r9w39a9r40000001657817946140",
+            width: 512,
+            height: 512,
+            position: new Vector3(6.8, 2.6, 4.2)
+        },
+        {
+            name: "image 2",
+            imgSrc: "images/002.png",
+            link: "",
+            width: 256,
+            height: 512,
+            position: new Vector3(6.8, 2.6, -4.2)
         },
         {
             name: "image 3",
