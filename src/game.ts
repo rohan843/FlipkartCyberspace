de06b510df9350ed9c04dd9919fa9b20a55e7ae4
Harshit Mittal <80350727+HarshitMittal01@users.noreply.github.com>
2022-07-16 23:50:35 +0530

    new furniture

--- src/game.ts
+++ src/game.ts
@@ -6,9 +6,10 @@ import { shopConfig1, shopConfig2 } from "./shop_config"
 import { addBirdField } from "./modules/birdField"
 import { addDoors } from "./modules/doors"
 import { addProjectorScreen} from "./modules/addProjector"
+import { addCaribeanWater, addGrassTile, addRockTile, addSandTile, addSideWalkTile, addTheFountainOfBrokenDreams, addTheFountainOfSpirits, addWoodTile } from "./modules/furniture"
 // import { addtelephoneBooth } from "./modules/furniture"
 
-
+addGrassTile(-7.60+15,0,64.08)
 addMuseumAt(8, 0, 8, museumConfig1)
 addMuseumAt(40, 0, 8, museumConfig1)
 addMuseumAt(56, 0, 24, museumConfig2)
@@ -23,7 +24,25 @@ addShopAt(24, 0, 104, shopConfig2)
 addBirdField()
 addDoors()
 addProjectorScreen()
+addTheFountainOfBrokenDreams(16,0,56)
+addTheFountainOfSpirits(16,0,62)
+// addCaribeanWater(8,-0.1,113)
+// addRockTile(-7.60,0,64.08)
+// addSandTile(-7.60+5,0,64.08)
+// addWoodTile(-7.60+10,0,64.08)
 
+// addSideWalkTile(-7.60+20,0,64.08)
+// addCaribeanWater(16,0,120)
+// addCaribeanWater(8,0,128)
+// addCaribeanWater(16,0,128)
+// addCaribeanWater(24,0,120)
+// addCaribeanWater(32,0,120)
+// addCaribeanWater(24,0,128)
+// addCaribeanWater(32,0,128)
+// addCaribeanWater(40,0,120)
+// addCaribeanWater(48,0,120)
+// addCaribeanWater(40,0,128)
+// addCaribeanWater(48,0,128)
 createDispenser(
     {
         position: new Vector3(8 + 5.5, 7.32, 8),
